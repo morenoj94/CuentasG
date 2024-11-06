59c5c5ff978514f597ec0357f8ed289a26a812a1
Jesus Moreno <ing.jmoreno94@gmail.com>
2024-11-05 21:03:09 -0500

    Primer commit con la estructura basica de la clase dominio y parte del script de la DB

---
+++ Domain/Custumer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Custumer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category CategoryId { get; set; }
+        public double SoupDiscount { get; set; }
+        public double MealDiscount { get; set; }
+        public double LastPay { get; set; }
+        public DateTime LastPayDay { get; set; }
+        public double Debt { get; set; }
+    }
+}
